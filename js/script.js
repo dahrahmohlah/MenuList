f371291bf6d5f74029c3cc2e1213a44e3c8fd968
Funto Daramola <fundaramola2013@gmail.com>
2018-04-12 12:50:36 +0100

    Added site navigation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 document.getElementById("footr").innerHTML = "<p>&copy;  " + new Date().getFullYear() + " All Rights Reserved.</p>";
+document.getElementById("nav01").innerHTML = "<ul id='menu'>" + "<li><a href='index.html'>Home</a></li>" + "<li><a href='customers.html'>Data</a></li>" + "<li><a href='about.html'>About</a></li>" + "</ul>";
